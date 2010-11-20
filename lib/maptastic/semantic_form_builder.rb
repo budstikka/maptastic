a7a10856033ce1e11d59292857656708188969f9
Peter Haza <peter.haza@gmail.com>
2010-11-20 04:33:28 +0100

    Support for labels above on the map (although they only have a visual effect)

--- lib/maptastic/semantic_form_builder.rb
+++ lib/maptastic/semantic_form_builder.rb
@@ -14,7 +14,11 @@ module Maptastic
   private
 
     def map_div_id(methods)
-      generate_html_id(methods.map(&:to_s).join('_') << '_map')
+      generate_html_id(map_div_id_prefix(methods))
+    end
+    
+    def map_div_id_prefix(methods)
+      methods.map(&:to_s).join('_') << '_map'
     end
 
     def map_input_id(method)
@@ -33,13 +37,14 @@ new MaptasticMap({
 "
       end
     end
-
+    
     def map_input(methods, options = {})
       options[:hint] ||= "Click to select a location, then drag the marker to position"
       inputs_html = methods.inject('') {|html, method| html << input(method, :id => map_input_id(method), :as => :hidden)}
       hint_html = inline_hints_for(methods.first, options)
+      label_html = label(options[:label], :label => options[:label], :input_name => map_div_id_prefix(methods) + '_input') if options[:label]
       map_container = @template.content_tag(:div, nil, :class => 'map', :id => map_div_id(methods))
-      map_html = @template.content_tag(:li,  Formtastic::Util.html_safe("#{map_container} #{hint_html.to_s} #{options[:skip_js] == true ? '' : map_js(methods, options[:zoom]).to_s}"))
+      map_html = @template.content_tag(:li,  Formtastic::Util.html_safe("#{label_html}#{map_container} #{hint_html.to_s} #{options[:skip_js] == true ? '' : map_js(methods, options[:zoom]).to_s}"))
 
       Formtastic::Util.html_safe(inputs_html + map_html)
     end
