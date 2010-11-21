ec3c0ec9722b953fe9a9d5b85d99c62ad86aae0d
Peter Haza <peter.haza@gmail.com>
2010-11-21 02:52:08 +0100

    Instead of zoom being just a property, support saving it to an element

--- lib/maptastic/semantic_form_builder.rb
+++ lib/maptastic/semantic_form_builder.rb
@@ -25,14 +25,14 @@ module Maptastic
       generate_html_id("map_#{method}")
     end
 
-    def map_js(methods, zoom, )
+    def map_js(methods, zoom)
       @template.content_tag("script", :type => "text/javascript") do
         "
  MaptasticMap.init({
     mapId: '#{map_div_id(methods)}',
     latInput: '#{map_input_id(methods.first)}',
     lngInput: '#{map_input_id(methods.last)}',
-    zoom : '#{zoom}'
+    zoomInput: '#{zoom ? map_input_id(zoom) : "null"}'
   });
 "
       end
@@ -49,6 +49,7 @@ module Maptastic
     def map_input(methods, options = {})
       options[:hint] ||= "Click to select a location, then drag the marker to position"
       inputs_html = methods.inject('') {|html, method| html << input(method, :id => map_input_id(method), :as => :hidden)}
+      inputs_html << input(options[:zoom], :id => map_input_id(options[:zoom]), :as => :hidden) if options[:zoom]
       hint_html = inline_hints_for(methods.first, options)
       label_html = label(options[:label], :label => options[:label], :input_name => map_div_id_prefix(methods) + '_input') if options[:label]
       map_container = @template.content_tag(:div, nil, :class => 'map', :id => map_div_id(methods))
@@ -59,7 +60,7 @@ module Maptastic
         text << hint_html
         text << (options[:skip_js] ? '' : map_js(methods, options[:zoom]).to_s)
         
-        text.join
+        Formtastic::Util.html_safe(text.join)
       end
 
       Formtastic::Util.html_safe(inputs_html + map_html)
